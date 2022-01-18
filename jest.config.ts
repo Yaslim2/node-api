a6e37355861857f0d2022952e46d5ecf28a065e3
Yaslim Soares <yaspqp@gmail.com>
2022-01-17 21:56:13 -0300

    chore: add jest and rules on eslint

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  roots: ['<rootDir/src'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}

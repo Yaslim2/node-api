91c0d8527eb30618f19b919b691f2cedad1cca0a
Yaslim Soares <yaspqp@gmail.com>
2022-01-19 22:36:21 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('should return an account on success', async () => {
+    await request(app).post('/api/signup')
+      .send({ name: 'Yaslim', email: 'yaslim@gmail.com', password: '1234', passwordConfirmation: '1234' })
+      .expect(200)
+  })
+})

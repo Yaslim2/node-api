1bccd03be0fb8612099798fb938150e1cdc411a0
Yaslim Soares <yaspqp@gmail.com>
2022-01-24 18:10:38 -0300

    test: ensure JwtAdapter throws if sign throw

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -20,4 +20,13 @@ describe('JWT Adapter', () => {
     const accessToken = await sut.encrypt('any-id')
     expect(accessToken).toBe('any-token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any-id')
+    await expect(promise).rejects.toThrow()
+  })
 })

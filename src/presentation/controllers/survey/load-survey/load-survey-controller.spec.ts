418f490ff41a883d9f7cc912f9e5cb2829caa88e
Yaslim Soares <yaspqp@gmail.com>
2022-01-28 19:16:59 -0300

    test: ensure LoadSurveyController returns 204 if LoadSurveys usecase returns an empty array

--- src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { SurveyModel, LoadSurveys } from './load-survey-protocols'
 import { LoadSurveyController } from './load-survey-controller'
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper'
 import MockDate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -69,6 +69,13 @@ describe('LoadSurvey Controller', () => {
 
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 204 if LoadSurveys returns an empty array', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise((resolve) => resolve([])))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(noContent())
   })
 
   test('Should return 500 if LoadSurveys throws', async () => {

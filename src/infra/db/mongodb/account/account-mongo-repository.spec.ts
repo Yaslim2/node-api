9e6199e4458b47f2bdc839528b75b2d56e16544b
Yaslim Soares <yaspqp@gmail.com>
2022-01-28 15:09:47 -0300

    test: ensure AccountMongoRepository will not found an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -110,5 +110,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any-email@email.com')
       expect(account.password).toBe('any-password')
     })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any-token')
+      expect(account).toBeFalsy()
+    })
   })
 })

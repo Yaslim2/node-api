b3a744ae77ddb50546ef470ce5438d0cc8d23a12
Yaslim Soares <yaspqp@gmail.com>
2022-01-28 19:16:56 -0300

    feat: ensure LoadSurveyController returns 204 if LoadSurveys usecase returns an empty array

--- src/presentation/controllers/survey/load-survey/load-survey-controller.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.ts
@@ -1,13 +1,13 @@
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import { Controller, HttpRequest, HttpResponse } from './load-survey-protocols'
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadSurveyController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (error) {
       return serverError(error)
     }

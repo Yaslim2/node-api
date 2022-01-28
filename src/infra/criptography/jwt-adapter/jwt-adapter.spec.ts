8a5ae5d64fd086b6f2249057e6a5c2a5eccb44f3
Yaslim Soares <yaspqp@gmail.com>
2022-01-28 14:45:39 -0300

    test: ensure JwtAdapter throws if verify throw

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -52,5 +52,14 @@ describe('JWT Adapter', () => {
       const value = await sut.decrypt('any-token')
       expect(value).toBe('any-value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any-token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })

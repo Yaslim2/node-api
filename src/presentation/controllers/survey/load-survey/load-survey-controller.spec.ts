467dd64f41bed175e8b35915c95face78fa6b22c
Yaslim Soares <yaspqp@gmail.com>
2022-01-28 19:08:09 -0300

    test: ensure LoadSurveyController returns 200 on success

--- src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
+++ src/presentation/controllers/survey/load-survey/load-survey-controller.spec.ts
@@ -1,5 +1,6 @@
 import { SurveyModel, LoadSurveys } from './load-survey-protocols'
 import { LoadSurveyController } from './load-survey-controller'
+import { ok } from '../../../helpers/http/http-helper'
 import MockDate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -61,5 +62,12 @@ describe('LoadSurvey Controller', () => {
 
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()))
   })
 })

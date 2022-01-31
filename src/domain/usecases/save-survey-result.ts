304515e2a772d7bf473b7bd91238143645e93da0
Yaslim Soares <yaspqp@gmail.com>
2022-01-31 18:11:17 -0300

    feat: add save survey result usecase

---
+++ src/domain/usecases/save-survey-result.ts
@@ -0,0 +1,12 @@
+import { SurveyResultModel } from '../models/survey-result'
+
+export type SaveSurveyResultModel = {
+  surveyId: string
+  accountId: string
+  answer: string
+  date: Date
+}
+
+export interface SaveSurveyResult {
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>
+}
